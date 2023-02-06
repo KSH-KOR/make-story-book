19983aa7f5f3714587146af719a175c83c3107a9
KSH-KOR <54264875+KSH-KOR@users.noreply.github.com>
2023-02-06 15:44:17 +0900

    study-page model created

--- lib/domain/models/story_page.dart
+++ lib/domain/models/story_page.dart
@@ -1,11 +1,28 @@
-class StoryPage {
-  final String storyPageCoverImageDownloadRrl;
-  final String prompt;
-  final String storyPageId;
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:my_english_story/service/story_book/story_page_constant.dart';
 
-  StoryPage({
-    required this.storyPageCoverImageDownloadRrl,
+class StudyPage {
+  final String pageDescription;
+  final String? pageImgUrl;
+  final int pageOrder;
+  final String prompt;
+  final String docId;
+
+  StudyPage({
+    required this.pageDescription,
+    required this.pageImgUrl,
+    required this.pageOrder,
     required this.prompt,
-    required this.storyPageId,
+    required this.docId, 
   });
+
+  factory StudyPage.fromSnapshot(
+          QueryDocumentSnapshot<Map<String, dynamic>> snapshot) =>
+      StudyPage(
+        docId: snapshot.id,
+        pageDescription: snapshot.data()[pageDescriptionFieldName].maybeEmpty(),
+        pageImgUrl: snapshot.data()[pageImgUrlFieldName],
+        pageOrder: snapshot.data()[pageOrderFieldName].maybeZero(),
+        prompt: snapshot.data()[storyPagePromptFieldName].maybeEmpty(),
+      );
 }
